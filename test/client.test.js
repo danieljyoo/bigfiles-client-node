ef4501a2bd06960fc7c2e51b44c7cea8a2b757a0
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-08-29 22:56:56 -0700

    added support for uploading small files

--- test/client.test.js
+++ test/client.test.js
@@ -10,18 +10,35 @@ const server = require('bigfiles-server-node')(process.env.BIGFILES_APIKEY)
 const TESTMIMETYPE = 'image/jpeg'
 const TESTBUCKET = 'test.funcmatic.com'
 const TESTKEY = 'image.test.jpg'
+
+// Small file upload tests
+const TESTSMALLFILE = path.join(__dirname, 'image.small.jpg')
+const TESTSMALLKEY = 'images.small.jpg'
+
 const APIKEY = process.env.BIGFILES_APIKEY
 
 describe('Multipart Upload File', () => {
   var uploadid = null
   
-  beforeEach(async () => {
-    var res = await server.start(TESTBUCKET, TESTKEY, TESTMIMETYPE)
-    uploadid = res.id
-  })
+  // beforeEach(async () => {
+
+  // })
 
   it ('should upload a > 5MB image file', async () => {
+    var res = await server.start(TESTBUCKET, TESTKEY, TESTMIMETYPE)
+    console.log("LARGERES", res)
+    var uploadid = res.id
+
     var res = await bigfiles.upload(uploadid, TESTFILE)
     console.log("RES", res)
   }, 5 * 60 * 1000)
+
+  it ('should upload a small < 5MB image file', async () => {
+    var res = await server.start(TESTBUCKET, TESTSMALLKEY, TESTMIMETYPE)
+    console.log("SMALLMULT", res)
+    var uploadid = res.id
+
+    var res = await bigfiles.upload(uploadid, TESTSMALLFILE, { content_type: TESTMIMETYPE })
+    console.log("SMALLRES", res)
+  })
 })
