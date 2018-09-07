2d52a8db0c2c10bd48800319b0fd3bf1b00b3eda
Daniel Jhin Yoo <danieljyoo@gmail.com>
2018-09-07 08:44:58 -0700

    added first cut of upload via a url to a file

--- test/client.test.js
+++ test/client.test.js
@@ -20,48 +20,71 @@ const TESTSMALLKEY = 'image.small.jpg'
 const TESTKEYSTREAM = 'image.stream.jpg'
 const TESTSMALLKEYSTREAM = 'image.small.stream.jpg'
 
+// URL
+const TESTKEYURL = 'image.url.jpg'
+const IMAGEURL = 'https://upload.wikimedia.org/wikipedia/commons/7/74/Earth_poster_large.jpg'
+const TESTSMALLKEYURL = 'image.small.url.jpg'
+const IMAGESMALLURL = 'https://image.pbs.org/video-assets/iZOsUzY-asset-mezzanine-16x9-8YZsCRv.jpg.focalcrop.1920x940.50.10.jpg'
+
 const APIKEY = process.env.BIGFILES_APIKEY
 
-describe('Upload File', () => {
-  var uploadid = null
+// describe('Upload File', () => {
+//   var uploadid = null
   
-  it ('should upload a > 5MB image file', async () => {
-    var res = await server.start(TESTBUCKET, TESTKEY, TESTMIMETYPE)
-    console.log("LARGERES", res)
-    var uploadid = res.id
+//   it ('should upload a > 5MB image file', async () => {
+//     var res = await server.start(TESTBUCKET, TESTKEY, TESTMIMETYPE)
+//     console.log("LARGERES", res)
+//     var uploadid = res.id
 
-    var res = await bigfiles.upload(uploadid, TESTFILE)
+//     var res = await bigfiles.upload(uploadid, TESTFILE)
+//     console.log("RES", res)
+//   }, 5 * 60 * 1000)
+
+//   it ('should upload a small < 5MB image file', async () => {
+//     var res = await server.start(TESTBUCKET, TESTSMALLKEY, TESTMIMETYPE)
+//     console.log("SMALLMULT", res)
+//     var uploadid = res.id
+
+//     var res = await bigfiles.upload(uploadid, TESTSMALLFILE, { content_type: TESTMIMETYPE })
+//     console.log("SMALLRES", res)
+//   })
+// })
+
+// describe('Upload Stream', () => {
+//   var uploadid = null
+
+//   it ('should upload a > 5MB image stream', async () => {
+//     var res = await server.start(TESTBUCKET, TESTKEYSTREAM, TESTMIMETYPE)
+//     console.log("LARGERES", res)
+//     var uploadid = res.id
+
+//     var res = await bigfiles.upload(uploadid, fs.createReadStream(TESTFILE))
+//     console.log("RES", res)
+//   }, 5 * 60 * 1000)
+
+//   it ('should upload a < 5MB image stream', async () => {
+//     var res = await server.start(TESTBUCKET, TESTSMALLKEYSTREAM, TESTMIMETYPE)
+//     console.log("SMALLMULT", res)
+//     var uploadid = res.id
+
+//     var res = await bigfiles.upload(uploadid, fs.createReadStream(TESTSMALLFILE), { content_type: TESTMIMETYPE })
+//     console.log("SMALLRES", res)
+//   })
+// })
+
+describe('Upload URL', () => {
+  it ('should upload a > 5MB image from a url', async () => {
+    var res = await server.start(TESTBUCKET, TESTKEYURL, TESTMIMETYPE)
+    var uploadid = res.id
+    console.log("uploadid", uploadid)
+    var res = await bigfiles.upload(uploadid, IMAGEURL, { content_type: TESTMIMETYPE })
     console.log("RES", res)
-  }, 5 * 60 * 1000)
-
-  it ('should upload a small < 5MB image file', async () => {
-    var res = await server.start(TESTBUCKET, TESTSMALLKEY, TESTMIMETYPE)
-    console.log("SMALLMULT", res)
+  }, 10 * 60 * 1000)
+  it ('should upload a < 5MB image using a url', async () => {
+    var res = await server.start(TESTBUCKET, TESTSMALLKEYURL, TESTMIMETYPE)
     var uploadid = res.id
-
-    var res = await bigfiles.upload(uploadid, TESTSMALLFILE, { content_type: TESTMIMETYPE })
-    console.log("SMALLRES", res)
-  })
-})
-
-describe('Upload Stream', () => {
-  var uploadid = null
-
-  it ('should upload a > 5MB image stream', async () => {
-    var res = await server.start(TESTBUCKET, TESTKEYSTREAM, TESTMIMETYPE)
-    console.log("LARGERES", res)
-    var uploadid = res.id
-
-    var res = await bigfiles.upload(uploadid, fs.createReadStream(TESTFILE))
+    console.log("uploadid", uploadid)
+    var res = await bigfiles.upload(uploadid, IMAGESMALLURL, { content_type: TESTMIMETYPE })
     console.log("RES", res)
-  }, 5 * 60 * 1000)
-
-  it ('should upload a < 5MB image stream', async () => {
-    var res = await server.start(TESTBUCKET, TESTSMALLKEYSTREAM, TESTMIMETYPE)
-    console.log("SMALLMULT", res)
-    var uploadid = res.id
-
-    var res = await bigfiles.upload(uploadid, fs.createReadStream(TESTSMALLFILE), { content_type: TESTMIMETYPE })
-    console.log("SMALLRES", res)
-  })
+  }, 10 * 60 * 1000)
 })
---
+++ test/writeablestream.test.js
@@ -0,0 +1,30 @@
+require('dotenv').config()
+const BigFilesWritable = require('../lib/writablestream')
+const request = require('request-promise-native')
+
+// Variables needed by the server library to start the multipart upload
+const server = require('bigfiles-server-node')(process.env.BIGFILES_APIKEY)
+const TESTMIMETYPE = 'image/jpeg'
+const TESTBUCKET = 'test.funcmatic.com'
+const TESTKEY = 'image.writeablestream.jpg'
+const TESTLARGEKEY = 'image.large.writeablestream.jpg'
+
+const IMAGEURL = 'https://image.pbs.org/video-assets/iZOsUzY-asset-mezzanine-16x9-8YZsCRv.jpg.focalcrop.1920x940.50.10.jpg'
+const IMAGELARGEURL = 'https://upload.wikimedia.org/wikipedia/commons/7/74/Earth_poster_large.jpg'
+
+describe('Request pipe', () => {
+  // it ('should pipe a request of a small file < 5MB', async () => {
+  //   var res = await server.start(TESTBUCKET, TESTKEY, TESTMIMETYPE)
+  //   var uploadid = res.id
+
+  //   var writable = new BigFilesWritable({ uploadid })
+  //   await request(IMAGEURL).pipe(writable)
+  // }, 30 * 1000)
+  it ('should pipe a request of a large file > 5MB', async () => {
+    var res = await server.start(TESTBUCKET, TESTLARGEKEY, TESTMIMETYPE)
+    var uploadid = res.id
+
+    var writable = new BigFilesWritable({ uploadid })
+    await request(IMAGELARGEURL).pipe(writable)
+  }, 30 * 1000)
+})
